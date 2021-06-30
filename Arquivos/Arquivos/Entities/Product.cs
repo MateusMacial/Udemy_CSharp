47482a277079b17b95a84a80d9152d355774c787
Mateus Macial <mateus_macial@hotmail.com>
2021-06-29 21:33:04 -0300

    Aula 29/06/2021

---
+++ Arquivos/Arquivos/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arquivos.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Arquivos/Arquivos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using Arquivos.Entities;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the path file: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\sumary.txt";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using(StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach(String line in lines)
+                    {
+                        string[] fields = line.Split(",");
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + ", $" + prod.TotalPrice().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Erro: ");
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
